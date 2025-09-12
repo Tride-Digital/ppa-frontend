3b72f346e8b00686b27c78256348faec0543a805
iamgagana12 <gagana@trbogen.com>
2025-09-12 12:45:17 +0530

    Changed config nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
     public: {
       adminAppUrl: 'https://qaadmin.planters.lk',
       registerUrl: 'https://qaadmin.planters.lk/register',
-      registerServiceUrl: 'http://localhost:3000/registerservice',
+      registerServiceUrl: 'https://qaadmin.planters.lk/registerservice',
     }
   },
 
