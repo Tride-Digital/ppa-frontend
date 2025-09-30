5780816a6a52615b6e592d57d82dcd336fbdc8f3
iamgagana12 <gagana@trbogen.com>
2025-09-30 13:57:33 +0530

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
     public: {
       adminAppUrl: 'https://qaadmin.planters.lk',
       registerUrl: 'https://qaadmin.planters.lk/register',
-      backendUrl: 'http://localhost:8000',
+      backendUrl: 'https://qaapippa.ecoderssl.com',
       registerServiceUrl: 'https://qaadmin.planters.lk/registerservice',
     }
   },
