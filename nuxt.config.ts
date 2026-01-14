3036380ccbd8086f2e5701019dc5876635120343
Jeewantha <jeewantha@trbogen.com>
2026-01-14 11:25:40 +0530

    Update runtime configuration URLs for production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,10 +42,10 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      adminAppUrl: 'https://qaadmin.planters.lk',
-      registerUrl: 'https://qaadmin.planters.lk/register',
-      backendUrl: 'https://qaapippa.ecoderssl.com',
-      registerServiceUrl: 'https://qaadmin.planters.lk/registerservice',
+      adminAppUrl: 'https://admin.planters.lk',
+      registerUrl: 'https://admin.planters.lk/register',
+      backendUrl: 'https://api.planters.lk',
+      registerServiceUrl: 'https://admin.planters.lk/registerservice',
     }
   },
 
